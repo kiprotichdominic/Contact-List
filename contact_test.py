55b691ca211391cc83c39a5848896300eec0730f
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-12 17:21:04 +0300

    initial commit

---
+++ contact_test.py
@@ -0,0 +1,87 @@
+import unittest
+import pyperclip
+from contact import Contact
+
+
+class TestContact(unittest.TestCase):
+    def setUp(self):
+        self.new_contact = Contact("Dominic", "Kiprotich", "0712345678", "kip@gmail.com")
+
+    def tearDown(self):
+        Contact.contact_list = []
+
+    def test_init(self):
+        self.assertEqual(self.new_contact.first_name, "Dominic")
+        self.assertEqual(self.new_contact.last_name, "Kiprotich")
+        self.assertEqual(self.new_contact.phone_number, "0712345678")
+        self.assertEqual(self.new_contact.email, "kip@gmail.com")
+
+    def test_save_contact(self):
+        self.new_contact.save_contact()
+        self.assertEqual(len(Contact.contact_list), 1)
+
+    def test_save_multiple_contact(self):
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "User", "0712345678", "test@user.com")
+        test_contact.save_contact()
+        self.assertEqual(len(Contact.contact_list), 2)
+
+    def test_delete_contact(self):
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "user", "0712345678", "test@user.com")  # new contact
+        test_contact.save_contact()
+
+        self.new_contact.delete_contact()  # deleting contact object
+        self.assertEqual(len(Contact.contact_list), 1)
+
+    def test_find_contact_by_number(self):
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "user", "0711223344", "test@user.com")  # new contact
+        test_contact.save_contact()
+
+        found_contact = Contact.find_by_number("0711223344")
+
+        self.assertEqual(found_contact.email, test_contact.email)
+
+    def test_contact_exists(self):
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "user", "0711223344", "test@user.com")  # new contact
+        test_contact.save_contact()
+
+        contact_exists = Contact.contact_exists("0711223344")
+
+        self.assertTrue(contact_exists)
+
+    def test_display_all_contacts(self):
+        self.assertEqual(Contact.display_contacts(), Contact.contact_list)
+
+    def test_copy_email(self):
+        self.new_contact.save_contact()
+        Contact.copy_email("0712345678")
+
+        self.assertEqual(self.new_contact.email, pyperclip.paste())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello_test.py
@@ -0,0 +1,12 @@
+# import unittest
+# from hello import Hello
+#
+#
+# class HelloPrint(unittest.TestCase):
+#     def test_hello(self):
+#         printed = "Hello World"
+#         self.assertEqual(printed)
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
